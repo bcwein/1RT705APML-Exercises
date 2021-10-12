995667ae713e7e413f52c56e901cf6fc66f33269
bcwein <bcwein343@gmail.com>
2021-10-12 10:47:34 +0200

    Initial Commit.

---
+++ Syllabus/syllabus.py
@@ -0,0 +1,10 @@
+"""Syllabus for Advanced Probabilistic Machine Learning.
+
+This file contains code for the whole syllabus of the APML Course at Uppsala
+University.
+"""
+
+# %%[markdown]
+# # Advanced Probabilistic Machine Learning Syllabus.
+# This file contains theory and python code for the APML 5 credit course
+# at the University of Uppsala fall 2021.
